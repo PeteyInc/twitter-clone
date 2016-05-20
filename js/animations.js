2bdc7aa989ee60afabc8425e5ce11b01b9ab07f1
Jared M. Peterson <jared@peteyinc.com>
2016-05-20 16:39:24 -0600

    updates

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,24 @@
+// 1. Always use proper indentation in your css and html
+// 2. An "#" selector or 'id' will override a '.' selector or 'class'
+// 3. A '.' selector will override a native selector, like 'div'
+// id = #
+// class = .
+
 $(document).ready(function() {
-  $('#tweet-controls').hide();
+  $('#tweet-controls').css('display', 'none');
+  $('.stats').css('display', 'none');
+  $('.reply').css('display', 'none');
+  $('.tweet-actions').css('display', 'none');
+
   $('textarea').on('click', function() {
     $(this).height('200%');
-    $('#tweet-controls').show();
+    $('#tweet-controls').toggle();
   });
+
+  $('.content').hover(function() {
+
+    $('.tweet-actions').toggle();
+  })
+
+
 });
