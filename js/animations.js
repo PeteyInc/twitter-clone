fd414282240197ffc3f9fd6cec7baf89cf4d22c1
Jared M. Peterson <jared@peteyinc.com>
2016-05-20 15:52:55 -0600

    first

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  
+  $('#tweet-controls').hide();
+  $('textarea').on('click', function() {
+    $(this).height('200%');
+    $('#tweet-controls').show();
+  });
 });
